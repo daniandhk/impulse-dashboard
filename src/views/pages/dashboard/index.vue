fcf1015d36429f1eb4e022dd813af35fe8d60e7c
Dani Andhika Permana <daniandhika03@gmail.com>
2021-04-29 20:33:09 +0700

    added dashboard roles

--- src/views/pages/dashboard/index.vue
+++ src/views/pages/dashboard/index.vue
@@ -1,17 +1,81 @@
 <script>
 import Layout from "../../layouts/main";
 import PageHeader from "@/components/page-header";
-
 /**
  * Dashboard component
  */
+import LaboranDashboard from "./dashboard-laboran";
+import StaffDashboard from "./dashboard-staff";
+import AslabDashboard from "./dashboard-aslab";
+import StudentDashboard from "./dashboard-student";
+import AsprakDashboard from "./dashboard-asprak";
+
+import store from '@/store';
+
 export default {
   components: {
     Layout,
-    PageHeader
+    PageHeader,
+    LaboranDashboard,
+    StaffDashboard,
+    AslabDashboard,
+    StudentDashboard,
+    AsprakDashboard
+  },
+  beforeMount: function(){
+    this.items[0].text = this.setRole(this.getRole)
+    switch(this.getRole) {
+      case "staff":
+        this.isLaboran = false;
+        this.isStaff = true;
+        this.isAsprak = false;
+        this.isAslab = false;
+        this.isStudent = false;
+        break;
+      case "aslab":
+        this.isLaboran = false;
+        this.isStaff = false;
+        this.isAsprak = false;
+        this.isAslab = true;
+        this.isStudent = false;
+        break;
+      case "asprak":
+        this.isLaboran = false;
+        this.isStaff = false;
+        this.isAsprak = true;
+        this.isAslab = false;
+        this.isStudent = false;
+        break;
+      case "laboran":
+        this.isLaboran = true;
+        this.isStaff = false;
+        this.isAsprak = false;
+        this.isAslab = false;
+        this.isStudent = false;
+        break;
+      case "student":
+        this.isLaboran = false;
+        this.isStaff = false;
+        this.isAsprak = false;
+        this.isAslab = false;
+        this.isStudent = true;
+        break;
+      default:
+        this.isLaboran = false;
+        this.isStaff = false;
+        this.isAsprak = false;
+        this.isAslab = false;
+        this.isStudent = false;
+    }
   },
   data() {
     return {
+      getRole: store.getters.getRoleUser,
+      isLaboran: false,
+      isStaff: false,
+      isAsprak: false,
+      isAslab: false,
+      isStudent: false,
       title: "Dashboard",
       items: [
         {
@@ -23,6 +87,24 @@ export default {
         }
       ]
     };
+  },
+  methods: {
+    setRole(role){
+      switch(role) {
+        case "staff":
+          return "Staff"
+        case "aslab":
+          return "Asisten Lab"
+        case "asprak":
+          return "Asisten Praktikum"
+        case "student":
+          return "Praktikan"
+        case "laboran":
+          return "Laboran"
+        default:
+          return role
+      }
+    }
   }
 };
 </script>
@@ -30,5 +112,12 @@ export default {
 <template>
   <Layout>
     <PageHeader :title="title" :items="items" />
+    <div>
+      <LaboranDashboard v-if="isLaboran"/>
+      <StaffDashboard v-if="isStaff"/>
+      <AslabDashboard v-if="isAslab"/>
+      <StudentDashboard v-if="isStudent"/>
+      <AsprakDashboard v-if="isAsprak"/>
+    </div>
   </Layout>
 </template>
