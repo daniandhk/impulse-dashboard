924e4750100b3ae2c0d90c49fd8b79f7e997bf88
Dani Andhika Permana <daniandhika03@gmail.com>
2021-08-30 14:22:53 +0700

    added start practicum asprak

--- src/api/asprak.js
+++ src/api/asprak.js
@@ -11,3 +11,19 @@ export function inputTest(data){
 export function deleteTest(id) {
     return httpAxios.delete(`/schedule/delete-test/${id}`);
 }
+
+export function inputScheduleTest(data){
+    return httpAxios({
+        url: '/schedule_test',
+        method: 'POST',
+        data: data
+    })
+}
+
+export function editScheduleTest(id, data){
+    return httpAxios({
+        url: `/schedule_test/${id}`,
+        method: 'PUT',
+        data: data
+    })
+}
