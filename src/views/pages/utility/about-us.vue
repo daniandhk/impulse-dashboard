0e0bb1a7e2897c337cb1686e19972edbfb226207
Dani Andhika Permana <daniandhika03@gmail.com>
2021-09-20 07:00:02 +0700

    added about us

--- src/views/pages/utility/about-us.vue
+++ src/views/pages/utility/about-us.vue
@@ -9,95 +9,101 @@ export default {
 <template>
   <div>
     <div class="home-btn d-none d-sm-block">
-      <a href="/" class="text-dark">
-        <i class="mdi mdi-home-variant h2"></i>
+      <a href="/" class="text-white">
+        <i class="mdi mdi-home-variant h2 text-primary"></i>
       </a>
     </div>
-    <div class="my-5 pt-sm-5">
+    <div class="my-1 pt-sm-5">
       <div class="container">
         <div class="row">
           <div class="col-12">
-            <div class="text-center">
-              <div class="mb-5">
-                <a href="/">
-                  <img src="@/assets/images/logo-dark.png" alt="logo" height="20" />
-                </a>
+            <div class="text-left">
+
+              <div class="text-center mb-5 mt-5">
+                <h3>Tentang IMPULSE</h3>
+                <p class="mt-3"> Information systeM for PracticUm and Laboratory courSE (IMPULSE). <br> Merupakan website yang berfungsi untuk mengelola penjadwalan,
+                monitoring dan pelaksanaan praktikum, sesuai dengan dengan kebutuhan praktikan,
+                asisten, laboran, Kaur. Lab dan pihak Fakultas Informatika, Telkom University.
+                Aplikasi ini diharapkan dapat mendukung pelaksanaan kegiatan operasional di
+                Laboratorium Informatika (IFLAB).</p>
               </div>
 
-              <div class="row justify-content-center">
-                <div class="col-sm-4">
-                  <div class="maintenance-img">
-                    <img
-                      src="@/assets/images/maintenance-bg.png"
-                      alt
-                      class="img-fluid mx-auto d-block"
-                    />
-                  </div>
-                </div>
-              </div>
-              <h3 class="mt-5">Site is Under Maintenance</h3>
-              <p>Please check back in sometime.</p>
+              <b-tabs nav-class="nav-tabs-custom1">
 
-              <div class="row">
-                <div class="col-md-4">
-                  <div class="mt-4 maintenance-box">
-                    <div class="p-3">
-                      <div class="avatar-sm mx-auto">
-                        <span class="avatar-title bg-soft-primary rounded-circle">
-                          <i class="mdi mdi-access-point-network font-size-24 text-primary"></i>
-                        </span>
-                      </div>
+                <b-tab title-link-class="p-3">
+                  <template v-slot:title>
+                    <a class="font-weight-bold active text-reset">Penanggung Jawab</a>
+                    </template>
+                    <p class="m-4">- Aulia Arif Wardana (KAUR LAB)</p>
+                </b-tab>
+                
+                <b-tab title-link-class="p-3">
+                  <template v-slot:title>
+                    <a class="font-weight-bold active text-reset">Mentor</a>
+                  </template>
+                    <p class="m-4">- Oku Dewi Noviyantika Putri</p>
+                    <p class="m-4">- Surya Mahadi</p>
+                    <p class="m-4">- Aditya Eka Bagaskara</p>
+                </b-tab>
 
-                      <h5 class="font-size-15 text-uppercase mt-4">Why is the Site Down?</h5>
-                      <p class="text-muted mb-0">
-                        There are many variations of passages of
-                        Lorem Ipsum available, but the majority have suffered alteration.
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-md-4">
-                  <div class="mt-4 maintenance-box">
-                    <div class="p-3">
-                      <div class="avatar-sm mx-auto">
-                        <span class="avatar-title bg-soft-primary rounded-circle">
-                          <i class="mdi mdi-clock-outline font-size-24 text-primary"></i>
-                        </span>
-                      </div>
-                      <h5 class="font-size-15 text-uppercase mt-4">What is the Downtime?</h5>
-                      <p class="text-muted mb-0">
-                        Contrary to popular belief, Lorem Ipsum is not
-                        simply random text. It has roots in a piece of classical.
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-md-4">
-                  <div class="mt-4 maintenance-box">
-                    <div class="p-3">
-                      <div class="avatar-sm mx-auto">
-                        <span class="avatar-title bg-soft-primary rounded-circle">
-                          <i class="mdi mdi-email-outline font-size-24 text-primary"></i>
-                        </span>
-                      </div>
-                      <h5 class="font-size-15 text-uppercase mt-4">Do you need Support?</h5>
-                      <p class="text-muted mb-0">
-                        If you are going to use a passage of Lorem
-                        Ipsum, you need to be sure there isn't anything embar..
-                        <a
-                          href="mailto:no-reply@domain.com"
-                          class="text-decoration-underline"
-                        >no-reply@domain.com</a>
-                      </p>
-                    </div>
-                  </div>
-                </div>
+                <b-tab title-link-class="p-3">
+                  <template v-slot:title>
+                    <a class="font-weight-bold active text-reset">Programmer</a>
+                  </template>
+                    <p class="m-4">- Dani Andhika Permana (Frontend) </p>
+                    <p class="m-4">- Edgar Vigo (Frontend) </p>
+                    <p class="m-4">- Muhammad Figo Akbar (Frontend) </p>
+                    <p class="m-4">- Fariz Muhammad Rizky (Backend) </p>
+                    <p class="m-4">- Indra Wahyudi (Backend) </p>
+                    <p class="m-4">- Muhammad Hafidh Raditya (Backend) </p>
+                </b-tab>
+                
+                <b-tab title-link-class="p-3">
+                  <template v-slot:title>
+                    <a class="font-weight-bold active text-reset">Network Administrator</a>
+                  </template>
+                    <p class="m-4">- Sya Raihan Heggi </p>
+                </b-tab>
+                
+                <b-tab title-link-class="p-3">
+                  <template v-slot:title>
+                    <a class="font-weight-bold active text-reset">Quality Assurance</a>
+                  </template>
+                  <p class="m-4">- Hafshah Haudli Windjatika </p>
+                  <p class="m-4">- Putri Ester Sumolang  </p>
+                  <p class="m-4">- Saskia Putri Ananda </p>
+                </b-tab>
+              </b-tabs> 
+
+              <div class="text-center">
+                <h3 class="mt-5">IMPULSE dalam naungan</h3>
               </div>
-              <!-- end row -->
+
+              <div class="row row-no-gutters justify-content-center text-center p-4">
+                  <div class="col-md-3 px-0 pr-2 pl-2" style="height:75%;">
+                      <a href="https://informatics.labs.telkomuniversity.ac.id/">
+                        <img class="img-fluid d-inline-flex align-items-center" src="@/assets/logo-lab-2.png" alt="">
+                      </a>
+                  </div>
+                  <div class="col-md-3 px-0 pr-2 pl-2 text-center" style="height:75%;">
+                      <a href="https://bif.telkomuniversity.ac.id/">
+                        <img class="img-fluid d-inline-flex align-items-center" src="@/assets/logo-fakultas.png" alt="">
+                      </a>
+                  </div>
+                  <div class="col-md-3 px-0 pr-2 pl-2" style="height:75%;">
+                      <a href="https://telkomuniversity.ac.id/">
+                        <img class="img-fluid d-inline-flex align-items-center" src="@/assets/logo-telkom.png" alt="">
+                      </a>
+                  </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
+    <div class="text-center">
+      <p> © 2021 Informatics Lab.</p>
+    </div>
   </div>
+  
 </template>
