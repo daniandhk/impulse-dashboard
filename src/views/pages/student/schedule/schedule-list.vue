247f7135c07f605c9655eea122fc20a4566ff25f
Dani Andhika Permana <daniandhika03@gmail.com>
2021-11-01 18:04:02 +0700

    changed button room

--- src/views/pages/student/schedule/schedule-list.vue
+++ src/views/pages/student/schedule/schedule-list.vue
@@ -300,7 +300,7 @@ export default {
                     <template v-slot:cell(room)="data">
                       <b-button
                           type="submit" 
-                          variant="primary"
+                          variant="outline-secondary"
                           @click=onClickShow(data)
                           size="sm"
                           style="min-width: 75px;" 
