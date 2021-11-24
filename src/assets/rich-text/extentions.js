818de4249d32ed71c6d8ad97171b26a6c3ab858a
Dani Andhika Permana <daniandhika03@gmail.com>
2021-11-24 16:05:08 +0700

    updated packages

---
+++ src/assets/rich-text/extentions.js
@@ -0,0 +1,41 @@
+export const TipTapExtensions = [
+    'Bold',
+    'Italic',
+    'Strike',
+    'Underline',
+    'Code',
+    'CodeBlock',
+    'CodeBlockHighlight',
+    'BulletList',
+    'OrderedList',
+    'ListItem',
+    'TodoList',
+    'HorizontalRule',
+    'Table',
+    'Link',
+    'Image',
+  ]
+  
+  export const QuasarTipTapExtensions = [
+    'OTitle',
+    'ODoc',
+    'OParagraph',
+    'OBlockquote',
+    'OTodoItem',
+    'OHeading',
+    'OAlignment',
+    'OLineHeight',
+    'OForeColor',
+    'OBackColor',
+    'OFontFamily',
+    'OIframe',
+    'ODiagram',
+    'OKatexBlock',
+    'OKatexInline',
+    'OFormatClear',
+  ]
+  
+  export const RecommendedExtensions = [
+    ...TipTapExtensions,
+    ...QuasarTipTapExtensions
+  ]
