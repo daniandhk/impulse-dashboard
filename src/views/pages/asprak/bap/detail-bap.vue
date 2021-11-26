0cc5059d36c8d59010fdc5b5318460c165c8678b
Dani Andhika Permana <daniandhika03@gmail.com>
2021-11-26 09:12:12 +0700

    fixed breadcrumb

--- src/views/pages/asprak/bap/detail-bap.vue
+++ src/views/pages/asprak/bap/detail-bap.vue
@@ -28,7 +28,7 @@ export default {
         },
         {
           text: "BAP",
-          href: "/asprak/bap"
+          href: "/asprak/schedule-bap"
         },
         {
           text: "Detail",
