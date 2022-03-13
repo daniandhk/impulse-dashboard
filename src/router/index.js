34466101bc126111af19c273927696ba6b42a8de
Dani Andhika Permana <daniandhika03@gmail.com>
2022-03-13 18:07:36 +0700

    revert logged out

--- src/router/index.js
+++ src/router/index.js
@@ -55,14 +55,6 @@ router.beforeEach((routeTo, routeFrom, next) => {
         next()
       })
       .catch(error => {
-        if(loggedUser){
-            api.logout(loggedUser.user_id).then(response => {
-                //
-            })
-            .catch(error => {
-                //
-            });
-        }
         store.dispatch('logOut')
         //console.log(error)
         redirectToLogin("expired")
